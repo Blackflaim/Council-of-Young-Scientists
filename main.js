bf875c769c13ddbed4e546ee11a35c4c42712e93
Artem Bondariev <bondar1605@gmail.com>
2026-05-19 17:17:12 +0300

    Add portfolio(projects.html)

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () =>
     '.program-card',
     '.news-card',
     '.article-card',
+    '.project-card',
     '.team-card',
     '.report-item',
     '.partner-logo'
