9df40552ff159f07dbbbf65967bb432254fc3ddf
Artem Bondariev <bondar1605@gmail.com>
2026-06-06 17:14:07 +0300

    vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        school: resolve(__dirname, 'school.html'),
+        grants: resolve(__dirname, 'grants.html'),
+        news: resolve(__dirname, 'news.html'),
+        team: resolve(__dirname, 'team.html'),
+        competitions: resolve(__dirname, "competitions.html")
+      }
+    }
+  }
+});
